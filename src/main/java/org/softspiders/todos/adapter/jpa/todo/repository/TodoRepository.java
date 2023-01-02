d9143e292d36052f591e00eebdf1bd59e6b3e1e8
AlexanderLapygin <alexanderlapygin@gmail.com>
2023-01-02 22:56:52 +0300

    feat: add annotation @Repository to repository

--- src/main/java/org/softspiders/todos/adapter/jpa/todo/repository/TodoRepository.java
+++ src/main/java/org/softspiders/todos/adapter/jpa/todo/repository/TodoRepository.java
@@ -3,7 +3,9 @@ package org.softspiders.todos.adapter.jpa.todo.repository;
 import org.softspiders.todos.adapter.jpa.todo.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     @Query("select t from TodoEntity t where t.completed is false")
     Iterable<TodoEntity> getPendingTodos();

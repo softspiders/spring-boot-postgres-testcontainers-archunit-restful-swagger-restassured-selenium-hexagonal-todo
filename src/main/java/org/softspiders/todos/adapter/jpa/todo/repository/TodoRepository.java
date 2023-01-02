3b3d3d8b17bbc392fe8662bf5ed54eb9a439fbb5
AlexanderLapygin <alexanderlapygin@gmail.com>
2023-01-03 00:04:39 +0300

    feat: remove unneeded annotation @Repository to repository

--- src/main/java/org/softspiders/todos/adapter/jpa/todo/repository/TodoRepository.java
+++ src/main/java/org/softspiders/todos/adapter/jpa/todo/repository/TodoRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     @Query("select t from TodoEntity t where t.completed is false")
     Iterable<TodoEntity> getPendingTodos();

b9d59dc03c6c0efc50e0ca26b38dfc31f657deaa
AlexanderLapygin <alexanderlapygin@gmail.com>
2023-02-01 00:59:21 +0300

    refactor: rename org.softspiders.todos package to org.softspiders.starters

--- adapter/src/main/resources/db/migration/V1__initial_schema.sql
+++
@@ -1,8 +0,0 @@
-create table todos
-(
-    id           varchar(100)    not null,
-    title        varchar(200) not null,
-    completed    boolean default false,
-    order_number int,
-    primary key (id)
-)

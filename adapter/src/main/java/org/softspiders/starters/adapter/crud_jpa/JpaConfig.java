249f7d92545c4adb26865eb671587e644cff8854
AlexanderLapygin <alexanderlapygin@gmail.com>
2023-02-04 19:11:32 +0300

    feat: upgrade spring boot version refactor: removal of excess

--- adapter/src/main/java/org/softspiders/starters/adapter/crud_jpa/JpaConfig.java
+++ adapter/src/main/java/org/softspiders/starters/adapter/crud_jpa/JpaConfig.java
@@ -1,6 +1,5 @@
 package org.softspiders.starters.adapter.crud_jpa;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;

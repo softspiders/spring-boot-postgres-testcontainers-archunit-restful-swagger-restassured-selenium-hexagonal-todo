2628b3fd5ca0f916a9e3765939f7f0643e52c8a5
AlexanderLapygin <alexanderlapygin@gmail.com>
2023-02-19 23:01:01 +0300

    refactor: adjust main and test settings

--- application/src/test/java/org/softspiders/starters/ApplicationTests.java
+++ application/src/test/java/org/softspiders/starters/ApplicationTests.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///todos"
+    "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///hexagonal"
 })
 class ApplicationTests {
 
